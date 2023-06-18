cfa30535be1177f8164eff6f5e0cef2082c99930
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2023-06-18 02:13:58 -0400

    Make strip_tags Importable as a library
    * ruff autosort imports
    
    Closes #9

--- strip_tags/cli.py
+++ strip_tags/cli.py
@@ -1,74 +1,6 @@
-from bs4 import BeautifulSoup
 import click
-import re
 
-# Elements that should be followed by a newline, derived from
-# https://www.w3.org/TR/2011/WD-html5-20110405/rendering.html#display-types
-NEWLINE_ELEMENTS = (
-    # display: block; default in the spec
-    "address",
-    "article",
-    "aside",
-    "blockquote",
-    "body",
-    "center",
-    "dd",
-    "dir",
-    "div",
-    "dl",
-    "dt",
-    "figure",
-    "figcaption",
-    "footer",
-    "form",
-    "h1",
-    "h2",
-    "h3",
-    "h4",
-    "h5",
-    "h6",
-    "header",
-    "hgroup",
-    "hr",
-    "html",
-    "legend",
-    "listing",
-    "menu",
-    "nav",
-    "ol",
-    "p",
-    "plaintext",
-    "pre",
-    "section",
-    "summary",
-    "ul",
-    "xmp",
-    # And <li> too, which default to display: list-item; in the spec:
-    "li",
-)
-
-DISPLAY_NONE_SELECTORS = [
-    "[hidden]",
-    "area",
-    "base",
-    "basefont",
-    "command",
-    "datalist",
-    "head",
-    "input[type=hidden]",
-    "link",
-    "menu[type=context]",
-    "meta",
-    "noembed",
-    "noframes",
-    "param",
-    "rp",
-    "script",
-    "source",
-    "style",
-    "track",
-    "title",
-]
+from strip_tags import strip_tags
 
 
 @click.command()
@@ -88,48 +20,5 @@ def cli(selectors, input, minify):
 
         cat input.html | strip-tags .entry .footer > output.txt
     """
-    soup = BeautifulSoup(input, "html5lib")
-    if not selectors:
-        selectors = ["body"]
-    output = []
-
-    # Remove elements with display: none
-    for none_selector in DISPLAY_NONE_SELECTORS:
-        for tag in soup.select(none_selector):
-            tag.decompose()
-
-    # Replace each image with its alt text
-    for img in soup.select("img[alt]"):
-        img.replace_with(img["alt"])
-
-    # Extract text from selected elements
-    for selector in selectors:
-        for tag in soup.select(selector):
-            # Output just the text content of this tag
-            output.append(tag.text)
-            if tag.name in NEWLINE_ELEMENTS:
-                output.append("\n")
-            # If the tag has a tail, output that too
-            if tag.tail:
-                output.append(tag.tail)
-
-    final = "".join(output).strip()
-    if minify:
-        final = minify_whitespace(final)
+    final = strip_tags(selectors, input, minify)
     click.echo(final)
-
-
-_whitespace_re = re.compile(r"\s+")
-
-
-def minify_whitespace(text):
-    def repl(m):
-        newline_count = m.group(0).count("\n")
-        if newline_count >= 2:
-            return "\n\n"
-        elif newline_count == 1:
-            return "\n"
-        else:
-            return " "
-
-    return _whitespace_re.sub(repl, text).strip()
--- tests/test_strip_tags.py
+++ tests/test_strip_tags.py
@@ -1,46 +1,59 @@
-from click.testing import CliRunner
-from strip_tags.cli import cli
 import pytest
+from click.testing import CliRunner
 
+from strip_tags import strip_tags
+from strip_tags.cli import cli
 
-@pytest.mark.parametrize(
-    "input, selectors, expected",
+TEST_PARAMETERS = (
+    ("<p>Hello <b>world</b></p>", [], "Hello world\n"),
+    ("<p>Hello <b>world</b></p>", ["p"], "Hello world\n"),
+    ("<p>Hello <b>world</b></p>", ["b"], "world\n"),
+    ("<div><span>span</span><b>b</b><i>i</i>", ["span", "i"], "spani\n"),
+    # Block level elements should have a newline
     (
-        ("<p>Hello <b>world</b></p>", [], "Hello world\n"),
-        ("<p>Hello <b>world</b></p>", ["p"], "Hello world\n"),
-        ("<p>Hello <b>world</b></p>", ["b"], "world\n"),
-        ("<div><span>span</span><b>b</b><i>i</i>", ["span", "i"], "spani\n"),
-        # Block level elements should have a newline
-        ("<div><h1>H1</h1><p>Para</p><pre>pre</pre>", ["h1", "pre"], "H1\npre\n"),
-        # Various ways whitespace should be stripped
-        ("Hello\nThis\nIs\nNewlines", ["--minify"], "Hello\nThis\nIs\nNewlines\n"),
-        ("Hello\nThis\n\n\nIs\nNewlines", ["-m"], "Hello\nThis\n\nIs\nNewlines\n"),
-        (
-            "Hello\nThis\n\t\t \t\n\nIs\nNewlines",
-            ["--minify"],
-            "Hello\nThis\n\nIs\nNewlines\n",
-        ),
-        (
-            "Hello  this \t has   \t spaces",
-            ["--minify"],
-            "Hello this has spaces\n",
-        ),
-        # Should remove script and style
-        (
-            "<script>alert('hello');</script><style>body { color: red; }</style>Hello",
-            [],
-            "Hello\n",
-        ),
-        # Test alt text replacement
-        (
-            '<img src="foo.jpg" alt="Foo"><img src="bar.jpg" alt="Bar">',
-            [],
-            "FooBar\n",
-        ),
+        "<div><h1>H1</h1><p>Para</p><pre>pre</pre>",
+        ["h1", "pre"],
+        "H1\npre\n",
+    ),
+    # Various ways whitespace should be stripped
+    (
+        "Hello\nThis\nIs\nNewlines",
+        ["--minify"],
+        "Hello\nThis\nIs\nNewlines\n",
+    ),
+    (
+        "Hello\nThis\n\n\nIs\nNewlines",
+        ["-m"],
+        "Hello\nThis\n\nIs\nNewlines\n",
+    ),
+    (
+        "Hello\nThis\n\t\t \t\n\nIs\nNewlines",
+        ["--minify"],
+        "Hello\nThis\n\nIs\nNewlines\n",
+    ),
+    (
+        "Hello  this \t has   \t spaces",
+        ["--minify"],
+        "Hello this has spaces\n",
+    ),
+    # Should remove script and style
+    (
+        "<script>alert('hello');</script><style>body { color: red; }</style>Hello",
+        [],
+        "Hello\n",
+    ),
+    # Test alt text replacement
+    (
+        '<img src="foo.jpg" alt="Foo"><img src="bar.jpg" alt="Bar">',
+        [],
+        "FooBar\n",
     ),
 )
+
+
+@pytest.mark.parametrize("input, selectors, expected", TEST_PARAMETERS)
 @pytest.mark.parametrize("use_i_option", (False, True))
-def test_strip(input, selectors, expected, use_i_option):
+def test_strip_cli(input, selectors, expected, use_i_option):
     runner = CliRunner()
     with runner.isolated_filesystem():
         args = selectors[:]
@@ -55,3 +68,22 @@ def test_strip(input, selectors, expected, use_i_option):
         result = runner.invoke(cli, args, **kwargs)
     assert result.exit_code == 0
     assert result.output == expected
+
+
+@pytest.mark.parametrize("input, selectors, expected", TEST_PARAMETERS)
+def test_strip_lib(input, selectors, expected):
+    # TODO: parameterize minify a different way
+    # list the ways minification gets flagged in selectors
+    minify_flags = set(["-m", "--minify"])
+    # determine if minification is called for
+    minify = any(obj in selectors for obj in minify_flags)
+
+    # remove `minify_flags` from `selectors`
+    selectors = [
+        selector for selector in selectors if selector not in minify_flags
+    ]
+
+    result = strip_tags(selectors, input, minify)
+    # TODO: cli testing requires expected output with `\n` appended
+    # strip `\n` from expected
+    assert result == expected[:-1]
