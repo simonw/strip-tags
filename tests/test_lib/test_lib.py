64398fbe089ac7b7806d4aa6c685ca6d18260ae7
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2023-06-24 15:58:45 -0400

    break lib tests out from cli tests

--- tests/test_lib/test_lib.py
+++ tests/test_lib/test_lib.py
@@ -1,35 +1,15 @@
-import pytest
-from click.testing import CliRunner
 import pathlib
-from strip_tags import strip_tags
-from strip_tags.cli import cli
+
+import pytest
 import yaml
+
+from strip_tags import strip_tags
 
 # load from tests.yaml
 TEST_PARAMETERS = [
     (d["input"], d["args"], d["expected"].strip())
-    for d in yaml.safe_load((pathlib.Path(__file__).parent / "tests.yaml").read_text())
+    for d in yaml.safe_load((pathlib.Path(__file__).parent / "cases.yaml").read_text())
 ]
-
-
-@pytest.mark.parametrize("input,args,expected", TEST_PARAMETERS)
-@pytest.mark.parametrize("use_i_option", (False, True))
-def test_strip_cli(input, args, expected, use_i_option):
-    runner = CliRunner()
-    with runner.isolated_filesystem():
-        # Copy to avoid modifying for later tests
-        args = args[:]
-        kwargs = {}
-        if use_i_option:
-            # Create temp file
-            with open("input.html", "w") as fp:
-                fp.write(input)
-                args.extend(["-i", "input.html"])
-        else:
-            kwargs["input"] = input
-        result = runner.invoke(cli, args, **kwargs)
-    assert result.exit_code == 0
-    assert result.output == expected + "\n"
 
 
 @pytest.mark.parametrize("input,args,expected", TEST_PARAMETERS)
@@ -70,4 +50,9 @@ def test_strip_lib(input, args, expected):
         keep_tags=keep_tags,
         all_attrs=all_attrs,
     )
+    assert result == expected
+    assert result == expected
+    assert result == expected
+    assert result == expected
+    assert result == expected
     assert result == expected
