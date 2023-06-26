513abd1d3df14a962e81c17353f8525359eea001
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2023-06-25 22:21:50 -0400

    remove check for html_files dir

--- tests/test_lib/test_lib.py
+++ tests/test_lib/test_lib.py
@@ -96,6 +96,126 @@ cases = [
         keep_tags=["a"],
         all_attrs=True,
     ),
+    Case(
+        id="lists bundle",
+        expected="""<ul>
+      <li>Source code</li>
+      <li>©</li>
+      <li>2002</li>
+      <li>2003</li>
+      <li>2004</li>
+      <li>2005</li>
+      <li>2006</li>
+      <li>2007</li>
+      <li>2008</li>
+      <li>2009</li>
+      <li>2010</li>
+      <li>2011</li>
+      <li>2012</li>
+      <li>2013</li>
+      <li>2014</li>
+      <li>2015</li>
+      <li>2016</li>
+      <li>2017</li>
+      <li>2018</li>
+      <li>2019</li>
+      <li>2020</li>
+      <li>2021</li>
+      <li>2022</li>
+      <li>2023</li>
+    </ul>""",
+        input="""<div id="ft">
+    <ul>
+      <li><a href="https://github.com/simonw/simonwillisonblog">Source code</a></li>
+      <li>&copy;</li>
+      <li><a href="/2002/">2002</a></li>
+      <li><a href="/2003/">2003</a></li>
+      <li><a href="/2004/">2004</a></li>
+      <li><a href="/2005/">2005</a></li>
+      <li><a href="/2006/">2006</a></li>
+      <li><a href="/2007/">2007</a></li>
+      <li><a href="/2008/">2008</a></li>
+      <li><a href="/2009/">2009</a></li>
+      <li><a href="/2010/">2010</a></li>
+      <li><a href="/2011/">2011</a></li>
+      <li><a href="/2012/">2012</a></li>
+      <li><a href="/2013/">2013</a></li>
+      <li><a href="/2014/">2014</a></li>
+      <li><a href="/2015/">2015</a></li>
+      <li><a href="/2016/">2016</a></li>
+      <li><a href="/2017/">2017</a></li>
+      <li><a href="/2018/">2018</a></li>
+      <li><a href="/2019/">2019</a></li>
+      <li><a href="/2020/">2020</a></li>
+      <li><a href="/2021/">2021</a></li>
+      <li><a href="/2022/">2022</a></li>
+      <li><a href="/2023/">2023</a></li>
+    </ul>
+</div>""",
+        keep_tags=["lists"],
+    ),
+    Case(
+        id="fix missing ul - https://github.com/simonw/strip-tags/pull/18#issuecomment-1600465929",
+        marks=[
+            pytest.mark.xfail(reason="https://github.com/simonw/strip-tags/issues/21")
+        ],
+        expected="""<ul>
+      <li>Source code</li>
+      <li>©</li>
+      <li>2002</li>
+      <li>2003</li>
+      <li>2004</li>
+      <li>2005</li>
+      <li>2006</li>
+      <li>2007</li>
+      <li>2008</li>
+      <li>2009</li>
+      <li>2010</li>
+      <li>2011</li>
+      <li>2012</li>
+      <li>2013</li>
+      <li>2014</li>
+      <li>2015</li>
+      <li>2016</li>
+      <li>2017</li>
+      <li>2018</li>
+      <li>2019</li>
+      <li>2020</li>
+      <li>2021</li>
+      <li>2022</li>
+      <li>2023</li>
+    </ul>""",
+        input="""<div id="ft">
+    <ul>
+      <li><a href="https://github.com/simonw/simonwillisonblog">Source code</a></li>
+      <li>&copy;</li>
+      <li><a href="/2002/">2002</a></li>
+      <li><a href="/2003/">2003</a></li>
+      <li><a href="/2004/">2004</a></li>
+      <li><a href="/2005/">2005</a></li>
+      <li><a href="/2006/">2006</a></li>
+      <li><a href="/2007/">2007</a></li>
+      <li><a href="/2008/">2008</a></li>
+      <li><a href="/2009/">2009</a></li>
+      <li><a href="/2010/">2010</a></li>
+      <li><a href="/2011/">2011</a></li>
+      <li><a href="/2012/">2012</a></li>
+      <li><a href="/2013/">2013</a></li>
+      <li><a href="/2014/">2014</a></li>
+      <li><a href="/2015/">2015</a></li>
+      <li><a href="/2016/">2016</a></li>
+      <li><a href="/2017/">2017</a></li>
+      <li><a href="/2018/">2018</a></li>
+      <li><a href="/2019/">2019</a></li>
+      <li><a href="/2020/">2020</a></li>
+      <li><a href="/2021/">2021</a></li>
+      <li><a href="/2022/">2022</a></li>
+      <li><a href="/2023/">2023</a></li>
+    </ul>
+</div>""",
+        keep_tags=["lists"],
+        selectors=["ul", "li", "ol"],
+    ),
 ]
 
 
